b99fd2387c6402102fefe61fe4fae2daeb0783f3
injoon <injoon5@naver.com>
2020-09-25 15:22:14 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+num = input('수는???')
+numnum = 1
+
+while num>1:
+    if(num%2==1):
+      num = num*3+1
+      print(num)
+      print()
+    else:
+        num = num/2
+        print(num)
+        print()
+        
