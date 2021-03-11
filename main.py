f63c7367ab227d30d72d112e8d79f0417cad533f
injoon <injoon5@naver.com>
2021-03-11 18:41:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ numnum = 1
 while num>1:
     if(num%2==1):
       num = num*3+1
-      print(num)
+      print(int(num))
       print()
       numnum = numnum+1
     else:
         num = num/2
-        print(num)
+        print(int(num))
         print()
         numnum = numnum+1
 print(numnum)
-        
